b8271914b1641db7f700793711d1f462b9b3fe82
Adam Dipinto <adam.dipinto@gmail.com>
2019-01-10 07:24:09 -0800

    Created a new call to pull user's repo data

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
     constructor() {
         this.client_id = '4ff86311094b3d378900';
         this.client_secret = '0ca88c20e55a0c124a04d026051fe2bfb749fe86';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
